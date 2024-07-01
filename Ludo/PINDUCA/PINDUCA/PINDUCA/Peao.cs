de125f5d48b57e41304b0f995bba686195f7284f
ArthurMedeiros01 <166410106+ArthurMedeiros01@users.noreply.github.com>
2024-06-30 21:47:18 -0300

    Add files via upload

---
+++ Ludo/PINDUCA/PINDUCA/PINDUCA/Peao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Peao
+{
+    public string cor;
+    public int identificador;
+    public int posicao;
+
+    public Peao(string cor, int identificador, int posicao)
+    {
+        this.posicao = posicao;
+        this.identificador = identificador;
+        this.cor = cor;
+
+    }
+
+
+
+    //mudar dps
+    public int RetornarCasa()
+    {
+        return posicao = 0;
+    }
+
+}
+
