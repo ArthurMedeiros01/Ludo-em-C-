651fba21e173a9b43c3e9a7261199c3bfc1c846b
ArthurMedeiros01 <166410106+ArthurMedeiros01@users.noreply.github.com>
2024-07-01 20:36:26 -0300

    Add files via upload

---
+++ Ludo/Ludo/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ludo;
+
+class program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Selecione a quantidade de jogadores: \nDigite 2 Caso jogue com 2 jogadores e 4 caso jogue com 4 jogadores");
+        int quantJogadores = int.Parse(Console.ReadLine());
+        if (quantJogadores == 2)
+        {
+            Console.WriteLine("Digite o nome do jogador da cor vermelha");
+            string nome1 = Console.ReadLine();
+            Jogador jogador1 = new Jogador("vermelho", 0, nome1);
+
+            Console.WriteLine("Digite o nome do jogador da cor amarela");
+            string nome2 = Console.ReadLine();
+            Jogador jogador3 = new Jogador("Amarelo", 2, nome2);
+
+
+            Jogador jogador2 = new Jogador("´Verde", 1, "...");
+            Jogador jogador4 = new Jogador("Azul", 3, "...");
+
+            Tabuleiro ludo = new Tabuleiro(jogador1, jogador3, quantJogadores);
+
+            while (jogador1.Vitoria() == false || jogador3.Vitoria() == false)
+            {
+                for (int i = 0; i < 4; i += 2)
+                {
+                    ludo.VerificarCaptura(ludo.jogadores[i].identificador, ludo.jogadores[i].peoes[i].identificador);
+                }
+                Console.ReadLine();
+            }
+        }
+        else if (quantJogadores == 4)
+        {
+            Console.WriteLine("Digite o nome do jogador da cor vermelha");
+            string nome1 = Console.ReadLine();
+            Jogador jogador1 = new Jogador("vermelho", 0, nome1);
+
+            Console.WriteLine("Digite o nome do jogador da cor verde");
+            string nome2 = Console.ReadLine();
+            Jogador jogador2 = new Jogador("Verde", 1, nome2);
+
+            Console.WriteLine("Digite o nome do jogador da cor amarelo");
+            string nome3 = Console.ReadLine();
+            Jogador jogador3 = new Jogador("Amarelo", 1, nome3);
+
+            Console.WriteLine("Digite o nome do jogador da cor azul");
+            string nome4 = Console.ReadLine();
+            Jogador jogador4 = new Jogador("Azul", 1, nome4);
+
+            Tabuleiro ludo = new Tabuleiro(jogador1, jogador2, jogador3, jogador4, quantJogadores);
+
+            //Partida 4 jogadores
+            while (ludo.jogadores[0].Vitoria() == false || ludo.jogadores[1].Vitoria() == false || ludo.jogadores[2].Vitoria() == false || ludo.jogadores[3].Vitoria() == false)
+            {
+                for (int i = 0; i < ludo.jogadores.Length; i++)
+                {
+                    Console.WriteLine($"Vez do jogador {ludo.jogadores[i].nome} ({ludo.jogadores[i].cor})");
+                    ludo.jogadores[i].LancarDados();
+
+                    ludo.jogadores[0].Vitoria();
+                    ludo.jogadores[1].Vitoria();
+                    ludo.jogadores[2].Vitoria();
+                    ludo.jogadores[3].Vitoria();
+                }
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Quantidade de jogadores inválida");
+        }
+
+
+    }
+}
---
+++ Ludo/Ludo/Tabuleiro.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo
+{
+    class Tabuleiro
+    {
+        public Jogador[] jogadores;
+
+        public Tabuleiro(Jogador jogador1, Jogador jogador2, Jogador jogador3, Jogador jogador4, int quantJogadores)
+        {
+
+            jogadores = new Jogador[quantJogadores];
+            jogadores[0] = jogador1;
+            jogadores[1] = jogador2;
+            jogadores[2] = jogador3;
+            jogadores[3] = jogador4;
+
+        }
+
+        public Tabuleiro(Jogador jogador1, Jogador jogador3, int quantJogadores)
+        {
+
+            jogadores = new Jogador[quantJogadores];
+            jogadores[0] = jogador1;
+
+            jogadores[1] = jogador3;
+
+
+        }
+
+
+
+        public bool VerificarCasaSegura(int posicaoDopeao)
+        {
+            if (posicaoDopeao == 1 || posicaoDopeao == 9 || posicaoDopeao == 14 || posicaoDopeao == 22 || posicaoDopeao == 27 || posicaoDopeao == 35 || posicaoDopeao == 40 || posicaoDopeao == 48 || posicaoDopeao == 0 || posicaoDopeao > 51)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int PosicaoNoTabuleiro(int idDoJogadorQueMoveu, int idDoJogadorParado, int idDoPeaoDoJogadorQueMoveu, int idDoPeaoParado)
+        {
+            int diferenca = idDoJogadorQueMoveu - idDoJogadorParado;
+            if (diferenca == 2 || diferenca == -2)
+            {
+
+                if (jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao > 26)
+                {
+                    return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao - 26;
+                }
+                else
+                {
+                    return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao + 26;
+                }
+
+            }
+            else if (diferenca == 3 || diferenca == -3)
+            {
+                if (diferenca == 3)
+                {
+                    if (jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao > 39)
+                    {
+                        return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao - 13;
+                    }
+                    else
+                    {
+                        if (jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao < 39 && jogadores[idDoJogadorParado].peoes[idDoPeaoParado].posicao < 39)
+                        {
+                            return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao - 13;
+                        }
+                        else
+                        {
+                            return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao + 39;
+                        }
+                    }
+                }
+                else
+                {
+                    if (jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao > 39)
+                    {
+                        return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao - 39;
+                    }
+                    else
+                    {
+                        return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao + 13;
+                    }
+                }
+            }
+            else
+            {
+                if (diferenca == 1)
+                {
+                    if (jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao > 39 && jogadores[idDoJogadorParado].peoes[idDoPeaoParado].posicao < 13)
+                    {
+                        return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao - 39;
+                    }
+                    else
+                    {
+                        return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao + 13;
+                    }
+                }
+                else
+                {
+
+                    if (jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao < 13 && jogadores[idDoJogadorParado].peoes[idDoPeaoParado].posicao > 39)
+                    {
+                        return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao + 39;
+                    }
+                    else
+                    {
+                        return jogadores[idDoJogadorQueMoveu].peoes[idDoPeaoDoJogadorQueMoveu].posicao - 13;
+                    }
+                }
+
+            }
+        }
+
+        public void VerificarCaptura(int idDoJogadorQueMoveu, int idDoPeaoDoJogadorQueMoveu)
+        {
+            for (int i = 0; i < jogadores.Length; i++)
+            {
+                if (i != idDoJogadorQueMoveu)
+                {
+                    for (int j = 0; j < jogadores[i].peoes.Length; j++)
+                    {
+                        if (PosicaoNoTabuleiro(idDoJogadorQueMoveu, i, idDoPeaoDoJogadorQueMoveu, j) == jogadores[i].peoes[j].posicao)
+                        {
+                            if (!VerificarCasaSegura(jogadores[i].peoes[j].posicao))
+                            {
+                                Console.WriteLine($"O peão {j} do jogador {i} foi capturado! \nParabens agora você pode fazer mais uma lançamento de dados");
+                                jogadores[i].peoes[j].posicao = 0;
+                                jogadores[idDoJogadorQueMoveu].LancarDados();
+                                
+                            } 
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
